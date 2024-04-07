9a2cc5a9cc229b7a816c4264d7d5cb419d592cda
Muhammad Chaudhry <chaudhrysayyam85@gmail.com>
2024-04-07 16:46:13 -0400

    gh-pages changes

--- src/App.js
+++ src/App.js
@@ -123,7 +123,7 @@ class App extends Component {
 
     // Important: Include the "basename" in Router, which is needed for deploying the React app to GitHub Pages
     return (
-      <Router basename="/bank-of-react-starter-code">
+      <Router basename="/bank-of-react">
         <div>
           <Navbar />
           <Route exact path="/" render={HomeComponent} />
