36aecc65de157fd069e4a77c64fd954215bc7080
Muhammad Chaudhry <chaudhrysayyam85@gmail.com>
2024-04-07 15:13:32 -0400

    Home css

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,17 +5,23 @@ The Home component is used to demonstrate the use of Link.
 ==================================================*/
 import React, { Component } from "react";
 import AccountBalance from "./AccountBalance";
+import "../styles/Home.css";
 
 class Home extends Component {
   render() {
     return (
-      <div className="container">
+      <div className="home">
         <div>
-          <img src="https://picsum.photos/200/200" alt="bank" />
-
-          <h1>Bank of React</h1>
+          <div className="wave"></div>
+          <div className="wave"></div>
+          <div className="wave"></div>
+        </div>
+        <div>
+          <h1 className="header">Bank of React</h1>
 
           <br />
+
+          <h4>Your Balance is:</h4>
           <AccountBalance accountBalance={this.props.accountBalance} />
         </div>
       </div>
